a13828c94bd49cd0986202d26ba7a3f033b8e593
myli1993 <62779585+myli1993@users.noreply.github.com>
2021-11-22 10:46:13 +0800

    Rename Eva/ehm_Evap_print.m to Eva module/ehm_Evap_print.m

--- Eva module/ehm_Evap_print.m
+++ Eva module/ehm_Evap_print.m
@@ -1,13 +1,13 @@
 function ehm_Evap_print(result)
-[~,R]=geotiffread('F:\文章\水文模型\data\1.2resample\dem_shimen_0.1.tif');
-info = geotiffinfo('F:\文章\水文模型\data\1.2resample\dem_shimen_0.1.tif');
+[~,R]=geotiffread('F:\鏂囩珷\姘存枃妯″瀷\data\1.2resample\dem_shimen_0.1.tif');
+info = geotiffinfo('F:\鏂囩珷\姘存枃妯″瀷\data\1.2resample\dem_shimen_0.1.tif');
 geoTags = info.GeoTIFFTags.GeoKeyDirectoryTag;
 result_label={'result.Rsq','result.nse','result.rmse','result.mae'};
 E_label={'E','Eb','Ei','Ep','Es','Et','Ew'};
 for i=1:length(result_label)
     for j=1:length(E_label)
         eval(['x=',result_label{i},'.',E_label{j},';']);
-        filename{i,j}=['F:\文章\水文模型\data\2.1evap\result\',result_label{i},'_',E_label{j},'.tif'];
+        filename{i,j}=['F:\鏂囩珷\姘存枃妯″瀷\data\2.1evap\result\',result_label{i},'_',E_label{j},'.tif'];
         geotiffwrite(filename{i,j},x,R,'GeoKeyDirectoryTag',geoTags);
     end
 end
